430bbde20dd5a5dd2ae92a69f7f91de77c99b306
Toshio Kuratomi <a.badger@gmail.com>
2020-05-03 17:02:48 -0700

    Minor cleanups to ansibulled
    * Add type annotations to private methods of the cli file to verify type
      checking is working
    * Fix wording and grammar in an exception message
    * Return early when there's nothing to do in the arg parsing functions
    * Docstrings for the cli file
    * Add type annotations for things used by the docs build script
      * type annotations for DepsFile and galaxy.CollectionDownloader
    * Use a namedtuple for complex return values from parsing dependency files

--- ansibulled/dependency_files.py
+++ ansibulled/dependency_files.py
@@ -12,6 +12,13 @@ We don't want to install backwards incompatible collections until the next major
 """
 
 import pkgutil
+from typing import Dict, NamedTuple, Optional
+
+
+class DependencyFileData(NamedTuple):
+    ansible_version: str
+    ansible_base_version: str
+    deps: Dict[str, str]
 
 
 class InvalidFileFormat(Exception):
@@ -27,14 +34,19 @@ def parse_pieces_file(pieces_file=None):
 
     contents = contents.decode('utf-8')
     # One collection per line, ignoring comments and empty lines
-    collections = [c.strip() for line in contents.split('\n')
-                   if (c := line.strip()) and not c.startswith('#')]
+    # TODO: PY3.8:
+    # collections = [c for line in contents.split('\n')
+    #                if (c := line.strip()) and not c.startswith('#')]
+    collections = [line.strip() for line in contents.split('\n')
+                   if line.strip() and not line.strip().startswith('#')]
     return collections
 
 
-def _parse_name_version_spec_file(filename):
-    deps = {}
-    ansible_base_version = acd_version = None
+def _parse_name_version_spec_file(filename: str) -> DependencyFileData:
+    deps: Dict[str, str] = {}
+    ansible_base_version: Optional[str] = None
+    acd_version: Optional[str] = None
+
     with open(filename, 'r') as f:
         for line in f:
             record = [entry.strip() for entry in line.split(':', 1)]
@@ -59,21 +71,42 @@ def _parse_name_version_spec_file(filename):
         raise InvalidFileFormat(f'{filename} was invalid.  It did not contain'
                                 ' required fields')
 
-    return acd_version, ansible_base_version, deps
+    return DependencyFileData(acd_version, ansible_base_version, deps)
 
 
 class DepsFile:
-    def __init__(self, deps_file):
+    """
+    File containing the collections which are part of an Ansible release.
+
+    A DepsFile contains a sequence of lines with a collection name, ": ", and then an exact
+    version of the collection.  It tracks the exact collection-versions which were installed
+    with a particular ansible version.
+
+    The deps file has two special lines which are not collections.  They are::
+
+        _ansible_version: X1.Y1.Z1
+        _ansible_base_version: X2.Y2.Z2
+
+    These are, respectively, the ansible version that was built and the ansible-base version which
+    it was built against.  Note that the ansible release will depend on a compatible version of that
+    ansible base version, not an exact dependency on that precise version.
+    """
+
+    def __init__(self, deps_file: str):
+        """
+        Create a :mod:`DepsFile`.
+
+        :arg deps_file: filename of the `DepsFile`.
+        """
         self.filename = deps_file
 
-    def parse(self):
-        """
-        Parse the deps from a dependency file
-        """
+    def parse(self) -> DependencyFileData:
+        """Parse the deps from a dependency file."""
         return _parse_name_version_spec_file(self.filename)
 
-    def write(self, acd_version, ansible_base_version, included_versions):
-        """Write a list of all the dependent collections included in this ACD release"""
+    def write(self, acd_version: str, ansible_base_version: str,
+              included_versions: Dict[str, str]):
+        """Write a list of all the dependent collections included in this ACD release."""
         records = []
         for dep, version in included_versions.items():
             records.append(f'{dep}: {version}')
