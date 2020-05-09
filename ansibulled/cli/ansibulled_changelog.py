0d975fee3a7037ddca64c80b624aa9840cf06042
Felix Fontein <felix@fontein.de>
2020-05-09 14:58:25 +0200

    Prepare moving ansible-changelog to ansibulled-changelog.

---
+++ ansibulled/cli/ansibulled_changelog.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# Author: Felix Fontein <felix@fontein.de>
+# License: GPLv3+
+# Copyright: Ansible Project, 2020
+"""Entrypoint to the ansibulled-changelog script."""
+
+def main():
+    pass
