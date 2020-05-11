1764b0c0e0cb9cc9cb2d694ad1e47c5e50a18f51
Toshio Kuratomi <a.badger@gmail.com>
2020-05-11 09:37:41 -0700

    Add pyre search path for ansibulled itself
    For pyre to resolve imports to modules that are in the toplevel
    directory search path as pyre, we have to specify the path that the
    ansibulled python package dir resides in as a search-path as well as as
    the source-directory.

--- lint-pyre.sh
+++ lint-pyre.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-poetry run pyre --source-directory ansibulled --search-path $(poetry run python -c 'from distutils.sysconfig import get_python_lib;print(get_python_lib())') --search-path stubs/
+poetry run pyre --source-directory ansibulled --search-path $(poetry run python -c 'from distutils.sysconfig import get_python_lib;print(get_python_lib())') --search-path stubs/ --search-path .
