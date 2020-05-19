0cd2c5c59e2507cdaecc5a93a4803f865c8356f2
Toshio Kuratomi <a.badger@gmail.com>
2020-05-18 23:41:16 -0700

    Add schemas for verifying and normalizing documentation.
    This schema covers all of the plugin documentation.
    
    It has been run on the complete set of Ansible-2.10 docs that
    ansible-doc --json is capable of producing at this time.
    
    The schema is intended to convert from invalid input to valid input
    whenever the valid input can be extrapolated with a high degree
    of accuracy.  In testing, there were only a few collections in
    Ansible-2.10 which failed with the converting schema.
    
    We can also generate a json schema from this schema.  Validating
    documentation using the json schema will be stricter, failing to
    validate whenever something is not as the schema desires.  It's probably
    useful to run the json schema on all new plugins to make sure you have
    the proper structure for your documentation.  The converting schema can
    be used to validate that the old content can be read by the docsite
    building tool and can also be used if you want to do something with the
    documentation data yourself.
    
    There are two layers for using the schema.  If you import from
    schemas.ansible_doc, you have some schemas which are optimized for
    working directly with the output from ansible-doc --json.  You can even
    pass the json strings you get from that command diectly into it.
    
    If you would like more control (for instance, if you want to publish
    documentation from the DOCUMENTATION string even if the RETURN or
    EXAMPLES string fails validation), you will want to use the lower level
    schemas in the plugin_docs, callback_docs, and module_docs python
    modules.

---
+++ ansibulled/schemas/plugin_docs.py
@@ -0,0 +1,135 @@
+# coding: utf-8
+# Author: Toshio Kuratomi <tkuratom@redhat.com>
+# License: GPLv3+
+# Copyright: Ansible Project, 2020
+"""Schemas for the plugin DOCUMENTATION data."""
+
+# Ignore Unitialized attribute errors because BaseModel works some magic
+# to initialize the attributes when data is loaded into them.
+# pyre-ignore-all-errors[13]
+
+import typing as t
+
+import pydantic as p
+
+from .base import (REQUIRED_ENV_VAR_F, JSONValueT, OPTION_TYPE_F, RETURN_TYPE_F,
+                   BaseModel, DeprecationSchema, DocSchema, LocalConfig,
+                   OptionsSchema, list_from_scalars,
+                   normalize_option_type_names, transform_return_docs)
+
+
+_SENTINEL = object()
+
+
+class OptionEnvSchema(BaseModel):
+    name: str = REQUIRED_ENV_VAR_F
+    deprecated: DeprecationSchema = p.Field({})
+    version_added: str = 'historical'
+
+
+class OptionIniSchema(BaseModel):
+    key: str
+    section: str
+    version_added: str = 'historical'
+
+
+class OptionVarsSchema(BaseModel):
+    name: str
+    version_added: str = 'historical'
+
+
+class ReturnSchema(BaseModel):
+    """Schema of plugin return data docs."""
+
+    description: t.List[str]
+    choices: t.List[str] = []
+    elements: str = OPTION_TYPE_F
+    returned: str = 'success'
+    sample: JSONValueT = ''
+    type: str = RETURN_TYPE_F
+    version_added: str = 'historical'
+
+    @p.validator('description', pre=True)
+    def list_from_scalars(cls, obj):
+        return list_from_scalars(obj)
+
+    @p.validator('type', 'elements', pre=True)
+    def normalize_types(cls, obj):
+        return normalize_option_type_names(obj)
+
+    @p.root_validator(pre=True)
+    def remove_example(cls, values):
+        """
+        Remove example in favor of sample.
+
+        Having both sample and example is redundant.  Many more plugins are using sample so
+        standardize on that.
+        """
+        example = values.get('example', _SENTINEL)
+
+        if example is not _SENTINEL:
+            if values.get('sample'):
+                raise ValueError('Cannot specify `example` if `sample` has been specified.')
+
+            values['sample'] = example
+            del values['example']
+
+        return values
+
+
+class InnerReturnSchema(ReturnSchema):
+    """Nested return schema which allows leaving out description."""
+
+    contains: t.Dict[str, 'InnerReturnSchema'] = {}
+
+    @p.root_validator(pre=True)
+    def allow_description_to_be_optional(cls, values):
+        # Doing this in a validator so that the json-schema will still flag it as an error
+        if 'description' not in values:
+            values['description'] = []
+        return values
+
+
+InnerReturnSchema.update_forward_refs()
+
+
+class PluginReturnSchema(ReturnSchema):
+    """Toplevel return schema."""
+
+    contains: t.Dict[str, InnerReturnSchema] = {}
+
+
+class PluginOptionsSchema(OptionsSchema):
+    env: t.List[OptionEnvSchema] = []
+    ini: t.List[OptionIniSchema] = []
+    suboptions: t.Dict[str, 'PluginOptionsSchema'] = {}
+    vars: t.List[OptionVarsSchema] = []
+
+
+PluginOptionsSchema.update_forward_refs()
+
+
+class PluginDocSchema(DocSchema):
+    options: t.Dict[str, PluginOptionsSchema] = {}
+
+
+class PluginSchema(BaseModel):
+    """Documentation of an Ansible plugin."""
+
+    class Config(LocalConfig):
+        fields = {'return_': 'return',
+                  }
+    doc: PluginDocSchema
+    examples: str = ''
+    metadata: t.Optional[t.Dict[str, t.Any]] = None
+    return_: t.Dict[str, PluginReturnSchema] = {}
+
+    @p.validator('return_', pre=True)
+    def transform_return(cls, obj):
+        return transform_return_docs(obj)
+
+    @p.validator('examples', pre=True)
+    def normalize_examples(cls, value):
+        if value is None:
+            value = ''
+        return value
