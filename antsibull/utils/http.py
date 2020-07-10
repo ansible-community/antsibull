0f9390a1637e62bee78650e1c542971a2c41a9d9
Felix Fontein <felix@fontein.de>
2020-07-11 01:58:50 +0200

    Write own async context manager.

--- antsibull/utils/http.py
+++ antsibull/utils/http.py
@@ -4,7 +4,6 @@
 """General functions for working with aiohttp."""
 
 import asyncio
-import contextlib
 import typing as t
 import warnings
 
@@ -25,50 +24,49 @@ def _format_call(command: str, args: t.Tuple[t.Any, ...], kwargs: t.Mapping[str,
     )
 
 
-@contextlib.asynccontextmanager
-async def retry_get(aio_session: 'aiohttp.client.ClientSession',
-                    *args,
-                    acceptable_error_codes: t.Optional[t.Iterable[int]] = None,
-                    max_retries: t.Optional[int] = None,
-                    **kwargs) -> t.AsyncGenerator[aiohttp.ClientResponse, None]:
-    flog = mlog.fields(func='retry_get')
-    flog.debug('Enter')
+class RetryGetManager:
+    response: t.Optional[aiohttp.ClientResponse]
 
-    # Handle default value for max_retries
-    lib_ctx = app_context.lib_ctx.get()
-    if max_retries is None:
-        max_retries = lib_ctx.max_retries
+    def __init__(self,
+                 aio_session: 'aiohttp.client.ClientSession',
+                 args: t.Tuple[t.Any, ...],
+                 kwargs: t.Mapping[str, t.Any],
+                 max_retries: int,
+                 acceptable_error_codes: t.Iterable[int],
+                 ):
+        self.aio_session = aio_session
+        self.args = args
+        self.kwargs = kwargs
+        self.max_retries = max_retries
+        self.acceptable_error_codes = acceptable_error_codes
+        self.call_string = _format_call('get', args, kwargs)
+        self.response = None
 
-    # Make sure max_retries is at least 1
-    max_retries = max(max_retries, 1)
+    async def __aenter__(self) -> aiohttp.ClientResponse:
+        flog = mlog.fields(func='RetryGetManager.__aenter__')
+        flog.debug('Enter')
 
-    # Run HTTP requests
-    call_string = _format_call('get', args, kwargs)
-    try:
         error_codes = []
-        for retry in range(max_retries):
-            flog.debug('Execute {0}'.format(call_string))
+        for retry in range(self.max_retries):
+            flog.debug('Execute {0}'.format(self.call_string))
             try:
-                async with aio_session.get(*args, **kwargs) as response:
-                    status_code = response.status
-                    flog.debug('Status code {0}'.format(status_code))
-                    if status_code < 400:
-                        flog.debug('Yield')
-                        yield response
-                        return
-                    if acceptable_error_codes is not None and status_code in acceptable_error_codes:
-                        flog.debug('Yield')
-                        yield response
-                        return
-                    error_codes.append(status_code)
+                response = await self.aio_session.get(*self.args, **self.kwargs)
+                status_code = response.status
+                flog.debug('Status code {0}'.format(status_code))
+                if status_code < 400 or status_code in self.acceptable_error_codes:
+                    self.response = response
+                    flog.debug('Leave')
+                    return response
+                error_codes.append(status_code)
+                response.close()
             except Exception as error:
                 flog.trace()
                 status_code = str(error)
                 error_codes.append(status_code)
 
-            failed = retry + 1 == max_retries
+            failed = retry + 1 == self.max_retries
             warnings.warn('{0} failed with status code {1}{2}'.format(
-                call_string,
+                self.call_string,
                 status_code,
                 ', finally failed.' if failed else ', retrying...'
             ))
@@ -79,6 +77,30 @@ async def retry_get(aio_session: 'aiohttp.client.ClientSession',
 
         flog.debug('Raise error')
         raise Exception('Repeated error when calling {0}: received status codes {1}'.format(
-            call_string, ', '.join([str(error) for error in error_codes])))
-    finally:
+            self.call_string, ', '.join([str(error) for error in error_codes])))
+
+    async def __aexit__(self, exc_type, exc, tb) -> None:
+        flog = mlog.fields(func='RetryGetManager.__aexit__')
+        flog.debug('Enter')
+        response = self.response
+        self.response = None
+        if response is not None:
+            response.close()
         flog.debug('Leave')
+
+
+def retry_get(aio_session: 'aiohttp.client.ClientSession',
+              *args,
+              acceptable_error_codes: t.Optional[t.Iterable[int]] = None,
+              max_retries: t.Optional[int] = None,
+              **kwargs) -> t.AsyncContextManager[aiohttp.ClientResponse]:
+    # Handle default value for max_retries
+    lib_ctx = app_context.lib_ctx.get()
+    if max_retries is None:
+        max_retries = lib_ctx.max_retries
+
+    # Make sure max_retries is at least 1
+    max_retries = max(max_retries, 1)
+
+    # pyre-ignore[7]: no idea how to make pyre see that RetryGetManager has correct type
+    return RetryGetManager(aio_session, args, kwargs, max_retries, acceptable_error_codes or ())
