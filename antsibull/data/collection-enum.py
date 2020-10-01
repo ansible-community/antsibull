7d1f60318ca6b1ea8434a3ab2912896f37fdf0d8
Felix Fontein <felix@fontein.de>
2020-10-01 23:02:25 +0200

    Lint script.

--- antsibull/data/collection-enum.py
+++ antsibull/data/collection-enum.py
@@ -23,6 +23,57 @@ from ansible.utils.collection_loader import AnsibleCollectionConfig
 from ansible.utils.plugin_docs import get_docstring
 
 
+def load_plugin(loader, plugin_type, plugin):
+    result = {}
+    try:
+        plugin_context = loader.find_plugin_with_context(
+            plugin, mod_type='.py', ignore_deprecated=True, check_aliases=True)
+        if not plugin_context.resolved:
+            result['error'] = 'Cannot find plugin'
+            return result
+        plugin_name = plugin_context.plugin_resolved_name
+        filename = plugin_context.plugin_resolved_path
+        collection_name = plugin_context.plugin_resolved_collection
+
+        result.update({
+            'plugin_name': plugin_name,
+            'filename': filename,
+            'collection_name': collection_name,
+        })
+
+        documentation, plainexamples, returndocs, metadata = get_docstring(
+            filename, fragment_loader, verbose=False,
+            collection_name=collection_name, is_module=(plugin_type == 'module'))
+
+        if documentation is None:
+            result['error'] = 'No valid documentation found'
+            return result
+
+        documentation['filename'] = filename
+        documentation['collection'] = collection_name
+
+        ansible_doc = {
+            'doc': documentation,
+            'examples': plainexamples,
+            'return': returndocs,
+            'metadata': metadata,
+        }
+
+        try:
+            json.dumps(ansible_doc)
+            result['ansible-doc'] = ansible_doc
+        except Exception as e:
+            result['error'] = (
+                'Cannot serialize documentation as JSON: %s' % to_native(e)
+            )
+    except Exception as e:
+        result['error'] = (
+            'Missing documentation or could not parse documentation: %s' % to_native(e)
+        )
+
+    return result
+
+
 def load_all_plugins(plugin_type, basedir, coll_filter):
     loader = getattr(plugin_loader, '%s_loader' % plugin_type)
 
@@ -36,59 +87,21 @@ def load_all_plugins(plugin_type, basedir, coll_filter):
     if coll_filter is None:
         paths = loader._get_paths_with_context()
         for path_context in paths:
-            plugin_list.update(doc.DocCLI.find_plugins(path_context.path, path_context.internal, plugin_type))
+            plugin_list.update(
+                doc.DocCLI.find_plugins(path_context.path, path_context.internal, plugin_type))
 
     doc.add_collection_plugins(plugin_list, plugin_type, coll_filter=coll_filter)
 
     result = {}
     for plugin in plugin_list:
-        result[plugin] = {}
-        try:
-            plugin_context = loader.find_plugin_with_context(plugin, mod_type='.py', ignore_deprecated=True, check_aliases=True)
-            if not plugin_context.resolved:
-                result[plugin]['error'] = 'Cannot find plugin'
-                continue
-            plugin_name = plugin_context.plugin_resolved_name
-            filename = plugin_context.plugin_resolved_path
-            collection_name = plugin_context.plugin_resolved_collection
-
-            result[plugin].update({
-                'plugin_name': plugin_name,
-                'filename': filename,
-                'collection_name': collection_name,
-            })
-
-            documentation, plainexamples, returndocs, metadata = get_docstring(
-                filename, fragment_loader, verbose=False,
-                collection_name=collection_name, is_module=(plugin_type == 'module'))
-
-            if documentation is None:
-                result[plugin]['error'] = 'No valid documentation found'
-                continue
-
-            documentation['filename'] = filename
-            documentation['collection'] = collection_name
-
-            ansible_doc = {
-                'doc': documentation,
-                'examples': plainexamples,
-                'return': returndocs,
-                'metadata': metadata,
-            }
-
-            try:
-                json.dumps(ansible_doc)
-                result[plugin]['ansible-doc'] = ansible_doc
-            except Exception as e:
-                result[plugin]['error'] = 'Cannot serialize documentation as JSON: %s' % to_native(e)
-        except Exception as e:
-            result[plugin]['error'] = 'Missing documentation or could not parse documentation: %s' % to_native(e)
+        result[plugin] = load_plugin(loader, plugin_type, plugin)
 
     return result
 
 
 def main(args):
-    parser = argparse.ArgumentParser(prog=args[0], description='Bulk extraction of Ansible plugin docs.')
+    parser = argparse.ArgumentParser(
+        prog=args[0], description='Bulk extraction of Ansible plugin docs.')
     parser.add_argument('args', nargs='?', help='Collection filter', metavar='collection_filter')
     parser.add_argument('--pretty', action='store_true', help='Pretty-print JSON')
     opt_help.add_basedir_options(parser)
