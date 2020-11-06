2843e9f589ec56edf0f4600f572e8f3c8eed900a
Felix Fontein <felix@fontein.de>
2020-11-06 07:17:36 +0100

    Update copyright headers.

--- sphinx_antsibull_ext/pygments_lexer.py
+++ sphinx_antsibull_ext/pygments_lexer.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=no-self-argument
 #
 # Copyright 2006-2017 by the Pygments team, see AUTHORS at
 # https://github.com/pygments/pygments/blob/3e1b79c82d2df318f63f24984d875fd2a3400808/AUTHORS
