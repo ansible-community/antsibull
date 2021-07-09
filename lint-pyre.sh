e41174778b94e09846e12537c3a1a20070eed5fb
Toshio Kuratomi <a.badger@gmail.com>
2021-07-09 10:34:42 -0700

    Fix type checking of with BooleanOptionalAction
    * Add a newer argparse stub file from typeshed
      The argparse stub that's being found by pyre right now does not know
      about BooleanOptionalArgument.  Grab a newer version so that we have
      that information:
      https://raw.githubusercontent.com/python/typeshed/master/stdlib/argparse.pyi
    
    * Switch lint runs to python-3.9
      The immediate reason for ths is so that the type checker runs on
      Python-3.9.  We're using argparse.BooleanOptionalAction which is only
      available in Python-3.9.  Even though we have compat code for
      Python-3.8, I couldn't figure out how to tell the type checker that
      the
    3.9 code won't ever be run when run on 3.8.  So pyre fails with an
      undefined attribute error.  Running on python-3.9 should workaround
      that.
    
    * Enhance the lint-pyre script for python-3.9.
      On python-3.9, poetry virtualenvs separate platlb (usually compiled
      python extensions) and purelib (pure python modules).  Get both
      directories from distutils and add them to pyre's searchpath.

--- lint-pyre.sh
+++ lint-pyre.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -e
-poetry run pyre --source-directory antsibull --source-directory sphinx_antsibull_ext --search-path $(poetry run python -c 'from distutils.sysconfig import get_python_lib;print(get_python_lib())') --search-path stubs/ --search-path . "$@"
+
+PURELIB=$(poetry run python -c 'from distutils.sysconfig import get_python_lib;print(get_python_lib(0))')
+PLATLIB=$(poetry run python -c 'from distutils.sysconfig import get_python_lib;print(get_python_lib(1))')
+poetry run pyre --source-directory antsibull --source-directory sphinx_antsibull_ext --search-path "$PURELIB" --search-path "$PLATLIB" --search-path stubs/ --search-path . "$@"
