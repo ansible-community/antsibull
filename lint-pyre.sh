98dd7d465e5c09d3ac4035a11c404df31cefebad
Toshio Kuratomi <a.badger@gmail.com>
2021-11-14 13:55:58 -0800

    Move source of ansibulled to a subdir.
    pyre prefers to be given the directory which holds your python package
    as the source-directory rather than the directory which is your python
    package.  However, if the directory given is the toplevel, pyre tends to
    get slow for me.  (I believe since it scans temporary directories that
    I have accumulated [downloads of all those ansible collections...]).
    Putting the source code in its own subdirectory is the way to fix that.

--- lint-pyre.sh
+++ lint-pyre.sh
@@ -3,4 +3,4 @@ set -e
 
 PURELIB=$(poetry run python -c 'from distutils.sysconfig import get_python_lib;print(get_python_lib(0))')
 PLATLIB=$(poetry run python -c 'from distutils.sysconfig import get_python_lib;print(get_python_lib(1))')
-poetry run pyre --source-directory antsibull --source-directory sphinx_antsibull_ext --search-path "$PURELIB" --search-path "$PLATLIB" --search-path stubs/ --search-path . "$@"
+poetry run pyre --source-directory src --search-path "$PURELIB" --search-path "$PLATLIB" --search-path stubs/ "$@"
