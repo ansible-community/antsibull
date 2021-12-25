821e1329ae617072c3847d906cd23d4ec76f17e7
Felix Fontein <felix@fontein.de>
2021-12-25 14:28:26 +0100

    Add file_check_content config which allows to skip overwriting files of smaller size that have identical content.

--- src/antsibull/write_docs.py
+++ src/antsibull/write_docs.py
@@ -5,6 +5,7 @@
 """Output documentation."""
 
 import asyncio
+import os
 import os.path
 import typing as t
 
@@ -46,6 +47,31 @@ async def copy_file(source_path: str, dest_path: str) -> None:
     flog.debug('Enter')
 
     lib_ctx = app_context.lib_ctx.get()
+    if lib_ctx.file_check_content > 0:
+        # Check whether the destination file exists and has the same content as the source file,
+        # in which case we won't overwrite the destination file
+        try:
+            stat_d = os.stat(dest_path)
+            if stat_d.st_size <= lib_ctx.file_check_content:
+                stat_s = os.stat(source_path)
+                if stat_d.st_size == stat_s.st_size:
+                    # Read both files and compare
+                    async with aiofiles.open(source_path, 'rb') as f_in:
+                        content_to_copy = await f_in.read()
+                    async with aiofiles.open(dest_path, 'rb') as f_in:
+                        existing_content = await f_in.read()
+                    if content_to_copy == existing_content:
+                        flog.debug('Skipping copy, since files are identical')
+                        return
+                    # Since we already read the contents of the file to copy, simply write it to
+                    # the destination instead of reading it again
+                    async with aiofiles.open(dest_path, 'wb') as f_out:
+                        f_out.write(content_to_copy)
+                    return
+        except FileNotFoundError:
+            # Destination (or source) file does not exist
+            pass
+
     async with aiofiles.open(source_path, 'rb') as f_in:
         async with aiofiles.open(dest_path, 'wb') as f_out:
             # TODO: PY3.8: while chunk := await f.read(lib_ctx.chunksize)
@@ -58,8 +84,32 @@ async def copy_file(source_path: str, dest_path: str) -> None:
 
 
 async def write_file(filename: str, content: str) -> None:
-    async with aiofiles.open(filename, 'w') as f:
+    flog = mlog.fields(func='write_file')
+    flog.debug('Enter')
+
+    content = content.encode('utf-8')
+
+    lib_ctx = app_context.lib_ctx.get()
+    if lib_ctx.file_check_content > 0 and len(content) <= lib_ctx.file_check_content:
+        # Check whether the destination file exists and has the same content as the one we want to
+        # write, in which case we won't overwrite the file
+        try:
+            stat = os.stat(filename)
+            if stat.st_size == len(content):
+                # Read file and compare
+                async with aiofiles.open(filename, 'rb') as f:
+                    existing_content = await f.read()
+                if existing_content == content:
+                    flog.debug('Skipping write, since file already contains the exact content')
+                    return
+        except FileNotFoundError:
+            # Destination file does not exist
+            pass
+
+    async with aiofiles.open(filename, 'wb') as f:
         await f.write(content)
+
+    flog.debug('Leave')
 
 
 def _render_template(_template: Template, _name: str, **kwargs) -> str:
