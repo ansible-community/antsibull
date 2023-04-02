2cc0f06159caf244ef84fb74eed1312a876c236d
Maxwell G <maxwell@gtmx.me>
2023-04-02 06:20:48 -0500

    add unit tests for validate-tags-file subcommand

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2023 Maxwell G <maxwell@gtmx.me>
+# SPDX-License-Identifier: GPL-3.0-or-later
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or
+# https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from pathlib import Path
+
+import pytest
+
+HERE = Path(__file__).resolve().parent
+TEST_DATA = HERE / 'test_data'
+
+@pytest.fixture
+def test_data_path() -> Path:
+    return TEST_DATA
