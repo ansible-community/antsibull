7466fe43b3431ed270fecb51e9f277a4c220babb
Maxwell G <maxwell@gtmx.me>
2023-04-08 21:02:13 +0000

    nox bump: clean old build artifacts

--- noxfile.py
+++ noxfile.py
@@ -285,7 +285,7 @@ def bump(session: nox.Session):
         version,
         external=True,
     )
-    session.run("hatch", "build")
+    session.run("hatch", "build", "--clean")
 
 
 @nox.session
