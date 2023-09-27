11581707b992fdee64a6e295b283dcf95689d8a1
Felix Fontein <felix@fontein.de>
2023-09-27 18:35:07 +0200

    Improve interface.
    Co-authored-by: Maxwell G <maxwell@gtmx.me>

--- src/antsibull/versions.py
+++ src/antsibull/versions.py
@@ -129,7 +129,9 @@ def feature_freeze_version(spec: str, collection_name: str) -> str:
     return _FeatureFreezeVersion(spec, collection_name).compute_new_spec()
 
 
-def load_constraints_if_exists(filename: str) -> dict[str, SemVerSpec]:
+def load_constraints_if_exists(
+    filename: str | os.PathLike[str],
+) -> dict[str, SemVerSpec]:
     """
     Load a constraints file, if it exists.
     """
