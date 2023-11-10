ansible-community/antsibull

commit 3427a98adde9dc9641fda6348d738068d7007d0e

Move ansible setuptools configuration into setup.cfg (#530)
* add setup.cfg python_metadata generator

* add generate-package-files subcommand

* add tests for generate-package-files subcommand

I had to use `git add -f tests/test_data/package-files/` as our
.gitignore excluded some of the test_Data files.

* CI: make nox.yml DRY and add check_package_files

* rewrite verify_package_files tests in Python

* tests: commit pristine `setup.py`s from sdists

The next commit will show any differences between the new setup.py
generation code and the old code.

* add LegacyBuildMetaMaker; make write-package-files DRY

We still want to build legacy setup.py for ansible 6, 7, and 8. Instead
of duplicating the old code in build_ansible_commands.py, add a
LegacyBuildMetaMaker class with the same interface as BuildMetaMaker.

Note that there's a few differences in the 8.0.0a1 setup.py. I have no
idea why there's extra entries that don't seem to exist in the original
source distribution, but the final wheel's file list is the same.

This change also moves the package file generation code into a function
that can be shared by the rebuild-single and generate-package-files
commands. Typing annotations are changed to StrPath for files where
possible.

* nox: add check_package_files(8.1.0_setup_cfg)

Temporarily test the setup.cfg generator with 8.1.0.

* Improve changelog entrys' formatting

Co-authored-by: Felix Fontein <felix@fontein.de>

* verify_package_files: add .txt extension to file lists

---------

Co-authored-by: Felix Fontein <felix@fontein.de>

src/antsibull/cli/antsibull_build.py
@@ -33,6 +33,7 @@ from antsibull_core.vendored._argparse_booleanoptionalaction import (  # noqa: E
 
 from ..build_ansible_commands import (  # noqa: E402
     build_single_command,
+    generate_package_files_command,
     prepare_command,
     rebuild_single_command,
 )
@@ -59,6 +60,7 @@ ARGS_MAP = {
     "validate-deps": validate_dependencies_command,
     "validate-tags": validate_tags_command,
     "validate-tags-file": validate_tags_file_command,
+    "generate-package-files": generate_package_files_command,
 }
 
 
@@ -142,7 +144,13 @@ def _check_release_build_directories(args: argparse.Namespace) -> None:
 
 
 def _normalize_release_build_options(args: argparse.Namespace) -> None:
-    if args.command not in ("prepare", "single", "rebuild-single"):
+    if args.command not in (
+        "prepare",
+        "single",
+        "rebuild-single",
+        "validate-tags",
+        "generate-package-files",
+    ):
         return
 
     compat_version_part = (
@@ -216,6 +224,19 @@ def _normalize_validate_tags_file_options(args: argparse.Namespace) -> None:
         return
     if not os.path.exists(args.tags_file):
         raise InvalidArgumentError(f"{args.tags_file} does not exist!")
+
+
+def _normalize_generate_package_files_options(args: argparse.Namespace) -> None:
+    if args.command not in ("generate-package-files",):
+        return
+    if not os.path.isdir(args.package_dir):
+        raise InvalidArgumentError(f"{args.package_dir} does not exist!")
+
+    args.collections_dir = args.collections_dir or os.path.join(
+        args.package_dir, "ansible_collections"
+    )
+    if not os.path.isdir(args.collections_dir):
+        raise InvalidArgumentError(f"{args.collections_dir} does not exist!")
 
 
 def parse_args(program_name: str, args: list[str]) -> argparse.Namespace:
@@ -390,30 +411,14 @@ def parse_args(program_name: str, args: list[str]) -> argparse.Namespace:
         " The tags data file in the sdist is always named 'tags.yaml'",
     )
 
-    rebuild_single_parser = subparsers.add_parser(
-        "rebuild-single",
-        parents=[build_write_data_parser, cache_parser, build_step_parser],
-        description="Rebuild a single-file Ansible from" " a dependency file",
-    )
-    rebuild_single_parser.add_argument(
-        "--sdist-dir",
-        default=".",
-        help="Directory to write the generated sdist tarball to",
-    )
-    rebuild_single_parser.add_argument(
+    package_file_parser = argparse.ArgumentParser(add_help=False)
+    package_file_parser.add_argument(
         "--debian",
         action="store_true",
         help="Include Debian/Ubuntu packaging files in"
         " the resulting output directory",
     )
-    rebuild_single_parser.add_argument(
-        "--sdist-src-dir",
-        help="Copy the files from which the source distribution is"
-        " created to the specified directory. This is mainly useful"
-        " for debugging antsibull-build",
-    )
-
-    rebuild_single_parser.add_argument(
+    package_file_parser.add_argument(
         "--tags-file",
         nargs="?",
         const="DEFAULT",
@@ -422,6 +427,28 @@ def parse_args(program_name: str, args: list[str]) -> argparse.Namespace:
         f" as {DEFAULT_FILE_BASE}-X.Y.Z-tags.yaml."
         " --tags-file takes an optional argument to change the filename."
         " The tags data file in the sdist is always named 'tags.yaml'",
+    )
+
+    rebuild_single_parser = subparsers.add_parser(
+        "rebuild-single",
+        parents=[
+            build_write_data_parser,
+            cache_parser,
+            build_step_parser,
+            package_file_parser,
+        ],
+        description="Rebuild a single-file Ansible from" " a dependency file",
+    )
+    rebuild_single_parser.add_argument(
+        "--sdist-dir",
+        default=".",
+        help="Directory to write the generated sdist tarball to",
+    )
+    rebuild_single_parser.add_argument(
+        "--sdist-src-dir",
+        help="Copy the files from which the source distribution is"
+        " created to the specified directory. This is mainly useful"
+        " for debugging antsibull-build",
     )
 
     subparsers.add_parser(
@@ -494,6 +521,21 @@ def parse_args(program_name: str, args: list[str]) -> argparse.Namespace:
     )
     validate_tags_file.add_argument("tags_file")
 
+    generate_package_files = subparsers.add_parser(
+        "generate-package-files",
+        description=generate_package_files_command.__doc__,
+        parents=[build_parser, build_step_parser, package_file_parser],
+    )
+    generate_package_files.add_argument(
+        "-p",
+        "--package-dir",
+        required=True,
+        help="Directory in which to write the package files",
+    )
+    generate_package_files.add_argument(
+        "-c", "--collections-dir", help="Defaults to {PACKAGE_DIR}/ansible_collections"
+    )
+
     parsed_args: argparse.Namespace = parser.parse_args(args)
 
     # Validation and coercion
@@ -506,6 +548,7 @@ def parse_args(program_name: str, args: list[str]) -> argparse.Namespace:
     _normalize_validate_tags_options(parsed_args)
     _normalize_release_rebuild_options(parsed_args)
     _normalize_validate_tags_file_options(parsed_args)
+    _normalize_generate_package_files_options(parsed_args)
 
     return parsed_args
 

.github/workflows/nox.yml
@@ -19,44 +19,63 @@ env:
   FORCE_COLOR: "1"
 
 jobs:
-  nox-lint:
+  nox:
     runs-on: ubuntu-latest
     defaults:
       run:
         working-directory: antsibull
+    strategy:
+      fail-fast: false
+      matrix:
+        include:
+          - session: test
+            python-versions: "3.9, 3.10, 3.11"
+            codecov: true
+            packages: ""
+
+          - session: "check_package_files(8.1.0)"
+            python-versions: "3.11"
+            codecov: true
+            packages: ""
+
+          - session: "check_package_files(7.5.0)"
+            python-versions: "3.11"
+            codecov: true
+            packages: ""
+
+          - session: "check_package_files(8.1.0_setup_cfg)"
+            python-versions: "3.11"
+            codecov: true
+            packages: ""
+
+          - session: lint
+            python-versions: "3.11"
+            codecov: false
+            packages: ""
+    name: "Run nox ${{ matrix.session }} session"
     steps:
       - name: Check out antsibull
         uses: actions/checkout@v3
         with:
           path: antsibull
+      - name: Install extra packages
+        if: "matrix.packages != ''"
+        run: |
+          sudo apt-get install -y ${{ matrix.packages }}
       - name: Setup nox
         uses: wntrblm/nox@2023.04.22
         with:
-          python-versions: "3.11"
-      - name: nox -e lint
+          python-versions: "${{ matrix.python-versions }}"
+      - name: "Run nox -e ${{ matrix.session }}"
         run: |
-          OTHER_ANTSIBULL_MODE=git nox -v -e lint
-  nox-test:
-    runs-on: ubuntu-latest
-    defaults:
-      run:
-        working-directory: antsibull
-    steps:
-      - name: Check out antsibull
-        uses: actions/checkout@v3
-        with:
-          path: antsibull
-      - name: Setup nox
-        uses: wntrblm/nox@2023.04.22
-        with:
-          python-versions: "3.9, 3.10, 3.11"
-      - name: nox -e test
-        run: |
-          OTHER_ANTSIBULL_MODE=git nox -v -e test
+          OTHER_ANTSIBULL_MODE=git nox -v -e "${{ matrix.session }}"
       - name: Report coverage
+        if: ${{ matrix.codecov }}
         run: |
           nox -v -e coverage
       - name: Upload coverage
+        if: ${{ matrix.codecov }}
         uses: codecov/codecov-action@v3
         with:
           directory: antsibull
+          name: "${{ matrix.session }}"

.reuse/dep5
@@ -1,5 +1,5 @@
 Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
 
-Files: changelogs/fragments/* .git-blame-ignore-revs
+Files: changelogs/fragments/* .git-blame-ignore-revs tests/test_data/package-files/*
 Copyright: Ansible Project
 License: GPL-3.0-or-later

noxfile.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 import contextlib
 import os
 import tempfile
+from collections.abc import Iterator, Sequence
 from pathlib import Path
 
 import nox
@@ -89,8 +90,23 @@ def test(session: nox.Session):
         "term-missing",
         *more_args,
         *session.posargs,
+        "tests",
         env={"COVERAGE_FILE": f"{covfile}", **session.env},
     )
+
+
+@contextlib.contextmanager
+def coverage_run(session: nox.Session) -> Iterator[tuple[str, dict[str, str]]]:
+    build_command = (
+        "coverage run -p --branch --source antsibull -m antsibull.cli.antsibull_build"
+    )
+    tmp = Path(session.create_tmp())
+    covfile = tmp / ".coverage"
+    cov_env = {"COVERAGE_FILE": f"{covfile}", **session.env}
+    yield build_command, cov_env
+    combined = map(str, tmp.glob(".coverage.*"))
+    session.run("coverage", "combine", *combined, env=cov_env)
+    session.run("coverage", "report", env=cov_env)
 
 
 @nox.session
@@ -107,11 +123,6 @@ def coverage_release(session: nox.Session):
     )
 
     tmp = Path(session.create_tmp())
-    covfile = tmp / ".coverage"
-    cov_env = {"COVERAGE_FILE": f"{covfile}", **session.env}
-    build_command = (
-        "coverage run -p --branch --source antsibull -m antsibull.cli.antsibull_build"
-    )
     posargs = session.posargs
     # Set default settings
     if not posargs:
@@ -129,23 +140,20 @@ def coverage_release(session: nox.Session):
         "git+https://github.com/ansible-collections/community.general",
         env={"ANSIBLE_COLLECTIONS_PATH": str(collections), **session.env},
     )
-    session.run(
-        "ansible-playbook",
-        "-vv",
-        "playbooks/build-single-release.yaml",
-        *posargs,
-        "-e",
-        f"antsibull_build_command={build_command!r}",
-        env={
-            "ANSIBLE_COLLECTIONS_PATH": str(collections),
-            "ANSIBLE_CALLBACK_RESULT_FORMAT": "yaml",
-            **cov_env,
-        },
-    )
-
-    combined = map(str, tmp.glob(".coverage.*"))
-    session.run("coverage", "combine", *combined, env=cov_env)
-    session.run("coverage", "report", env=cov_env)
+    with coverage_run(session) as (build_command, cov_env):
+        session.run(
+            "ansible-playbook",
+            "-vv",
+            "playbooks/build-single-release.yaml",
+            *posargs,
+            "-e",
+            f"antsibull_build_command={build_command!r}",
+            env={
+                "ANSIBLE_COLLECTIONS_PATH": str(collections),
+                "ANSIBLE_CALLBACK_RESULT_FORMAT": "yaml",
+                **cov_env,
+            },
+        )
 
 
 @nox.session
@@ -222,6 +230,63 @@ def typing(session: nox.Session):
         "stubs/",
         *additional_libraries,
     )
+
+
+@nox.session
+@nox.parametrize(
+    ["major", "version", "extra_args"],
+    [
+        ["8", "8.1.0", []],
+        ["7", "7.5.0", []],
+        [
+            "8",
+            "8.1.0",
+            [
+                "--package-dir=tests/test_data/package-files/force_setup_cfg",
+                "--force-generate-setup-cfg",
+            ],
+        ],
+    ],
+    ["8.1.0", "7.5.0", "8.1.0_setup_cfg"],
+)
+def check_package_files(
+    session: nox.Session, major: str, version: str, extra_args: Sequence[str]
+) -> None:
+    install(session, ".[coverage]", *other_antsibull(), editable=True)
+    tmp = session.create_tmp()
+    build_data = Path(tmp, "ansible-build-data")
+    session.run_always(
+        "git",
+        "clone",
+        "--depth=1",
+        "https://github.com/ansible-community/ansible-build-data",
+        str(build_data),
+        external=True,
+    )
+
+    Path("tests/.cache").mkdir(exist_ok=True)
+    session.run(
+        "python",
+        "tests/verify_package_files.py",
+        "download",
+        "--no-force-dl",
+        version,
+    )
+    with coverage_run(session) as (_, cov_env):
+        session.run(
+            "coverage",
+            "run",
+            "-p",
+            "--branch",
+            "--source=antsibull",
+            "tests/verify_package_files.py",
+            "check",
+            f"--data-dir={build_data / major}",
+            version,
+            *session.posargs,
+            *extra_args,
+            env=cov_env,
+        )
 
 
 def _repl_version(session: nox.Session, new_version: str):

pyproject.toml
@@ -89,6 +89,7 @@ typing = [
     "types-aiofiles",
     "types-docutils",
     "types-PyYAML",
+    "types-setuptools",
 ]
 dev = [
     # Used by nox sessions
@@ -102,6 +103,17 @@ dev = [
 
 [tool.isort]
 profile = "black"
+extend_skip = [
+    "tests/test_data",
+]
+
+[tool.black]
+extend-exclude = '''
+(
+^/tests/test_data/.*
+| ^/tests/.cache/.*
+)
+'''
 
 [tool.coverage.paths]
 source = [

commit e651c6dbb16aa933e36ca4e1a9cd170fd9dba7e0

fix normalization of validate-tags subcommand options
The build options do not apply validate-tags command. I mistakenly
applied this function to the validate-tags subcommand in
3427a98adde9dc9641fda6348d738068d7007d0e. This caused a traceback.

Fixes: 3427a98adde9dc9641fda6348d738068d7007d0e

src/antsibull/cli/antsibull_build.py
@@ -148,7 +148,6 @@ def _normalize_release_build_options(args: argparse.Namespace) -> None:
         "prepare",
         "single",
         "rebuild-single",
-        "validate-tags",
         "generate-package-files",
     ):
         return