8d3cc9dbbece3c18f775100f566f4abd94564d02
Felix Fontein <felix@fontein.de>
2024-02-07 08:53:09 +0100

    Allow for parametrization.
    Note that tmp_path isn't good here since it won't be cleaned up since the last run.
    So you can end up with the command being broken (exiting with 0 but not writing any
    files) and the tests still passing locally.

--- tests/test_announcements.py
+++ tests/test_announcements.py
@@ -8,20 +8,39 @@ from __future__ import annotations
 import filecmp
 from pathlib import Path
 
+import pytest
+
 from antsibull.cli.antsibull_build import run
 
+ANNOUNCEMENT_TESTS = [
+    ("7.4.0", "announce-7.4.0"),
+]
 
-def test_announcements_command(test_data_path: Path, tmp_path: Path) -> None:
+
+@pytest.mark.parametrize("version, directory", ANNOUNCEMENT_TESTS)
+def test_announcements_command(
+    test_data_path: Path,
+    tmp_path_factory: pytest.TempPathFactory,
+    version: str,
+    directory: str,
+) -> None:
+    result_path = tmp_path_factory.mktemp(f"announcements-{version}")
     ret = run(
         [
             "antsibull-build",
             "announcements",
             f"--data-dir={test_data_path}",
-            f"-O{tmp_path}",
-            "7.4.0",
+            f"-O{result_path}",
+            version,
         ]
     )
     assert ret == 0
-    expected = test_data_path / "announce-7.4.0"
+    expected = test_data_path / directory
+    expected_files = set()
     for file in expected.iterdir():
-        assert filecmp.cmp(file, tmp_path / file.name)
+        assert filecmp.cmp(file, result_path / file.name)
+        expected_files.add(file.name)
+    for file in result_path.iterdir():
+        assert (
+            file.name in expected_files
+        ), f"{file.name} should not have been generated"
