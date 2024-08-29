1af2e184cfa6e336280eccd619a880cb37481347
Felix Fontein <felix@fontein.de>
2024-08-29 07:37:00 +0200

    Forgot to reformat.

--- src/antsibull/collection_meta.py
+++ src/antsibull/collection_meta.py
@@ -75,7 +75,9 @@ class RemovalInformation(p.BaseModel):
             raise ValueError("new_name must be provided if reason is renamed")
         if self.redirect_replacement_version is not None and self.version != "TBD":
             if self.redirect_replacement_version >= self.version:
-                raise ValueError("redirect_replacement_version must be smaller than version")
+                raise ValueError(
+                    "redirect_replacement_version must be smaller than version"
+                )
         return self
 
     @p.model_validator(mode="after")
